0748028a3bbb2fd2bb3c54618606eecc09cda7c8
Ayush Kumar Maharana <ayushmoharana13@gmail.com>
2026-08-26 22:03:37 +0530

    Update API base URL and improve request interceptor

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({
+  baseURL: 'https://careerboost-ai-1-42fh.onrender.com'
+})
 
-api.interceptors.request.use(cfg => {
+api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
-  if (token) cfg.headers.Authorization = `Bearer ${token}`
-  return cfg
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
 })
 
 export default api
